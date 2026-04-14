0748202ca7b142db93a9902ed136f1a2ab220fba
Daniel Negrinho Moreira <daniel.moreira@HFCP.CORP>
2026-04-14 12:19:26 -0300

    versão 1.00.131

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ async function startBot() {
             // Definimos o modelo GLOBALMENTE com a API v1 para evitar o erro 404
             global.geminiModel = genAI.getGenerativeModel(
             { model: "gemini-1.5-pro" }, 
-            { apiVersion: 'v1beta' } // MUDAR DE 'v1' PARA 'v1beta'
+            { apiVersion: 'v1' } // MUDAR DE 'v1' PARA 'v1beta'
             );
             console.log("✅ Sistema Gemini pronto e estável.");
         }
@@ -335,7 +335,7 @@ Sua resposta:`;
 
                    const model = genAI.getGenerativeModel(
                                 { model: "gemini-1.5-pro" }, 
-                                { apiVersion: 'v1beta' } 
+                                { apiVersion: 'v1' } 
                             );
                     const result = await model.generateContent(prompt);
                     const iaResponse = result.response.text().trim();
